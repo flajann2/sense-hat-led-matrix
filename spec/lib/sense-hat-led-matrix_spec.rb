0cd8953ca60ff4606b8b483ca5395838928b6ab1
Fred Mitchell <fred.mitchell@gmx.de>
2016-05-29 20:16:07 +0200

    added pry

--- spec/lib/sense-hat-led-matrix_spec.rb
+++ spec/lib/sense-hat-led-matrix_spec.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+include SenseHatAPI
 
 describe SenseHat do
   before(:each) do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,7 +98,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
-require 'sense-had-led-matrix'
+require 'sense-hat-led-matrix'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
